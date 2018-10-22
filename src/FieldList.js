00a38de21759c9b1fc6960585b8d64591f33f635
alexsinfarosa <as898@cornell.edu>
2018-10-22 13:37:51 -0400

    added dialog component when field is deleted

--- src/FieldList.js
+++ src/FieldList.js
@@ -14,6 +14,11 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
 
 import format from "date-fns/format";
 
@@ -34,6 +39,18 @@ const styles = theme => ({
 });
 
 class FieldList extends Component {
+  state = {
+    open: false,
+    id: null
+  };
+
+  handleClickOpen = id => {
+    this.setState({ open: true, id });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
   render() {
     const { classes } = this.props;
     return (
@@ -98,7 +115,7 @@ class FieldList extends Component {
                         <ListItemSecondaryAction>
                           <IconButton
                             aria-label="Delete"
-                            onClick={() => deleteField(field.id)}
+                            onClick={() => this.handleClickOpen(field.id)}
                           >
                             <DeleteIcon />
                           </IconButton>
@@ -107,6 +124,33 @@ class FieldList extends Component {
                     </div>
                   ))}
                 </List>
+                <Dialog
+                  open={this.state.open}
+                  onClose={this.handleClose}
+                  aria-labelledby="alert-dialog-delete-field"
+                  aria-describedby="alert-dialog-delete-selected-field"
+                  hideBackdrop={true}
+                >
+                  <DialogTitle id="alert-dialog-title">
+                    Are you sure you want to delete this field?
+                  </DialogTitle>
+                  <DialogContent />
+                  <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                      Undo
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        deleteField(this.state.id);
+                        this.handleClose();
+                      }}
+                      color="primary"
+                      autoFocus
+                    >
+                      Yes
+                    </Button>
+                  </DialogActions>
+                </Dialog>
               </Grid>
             </Grid>
           );
